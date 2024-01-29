ec3522f442ecb499026cb6ccdf1d44525211192c
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-29 17:05:06 -0500

    Add divide method and test matchers to calculator.spec.ts

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -7,4 +7,21 @@ describe('Test For Calculator', () => {
     expect(calculator.multiply(2, 3)).toBe(6);
   }
   );
+
+  it('#divide should return 3', () => {
+    const calculator = new Calculator();
+    expect(calculator.divide(6, 2)).toBe(3);
+  }
+  );
+
+  it('test matchers',() => {
+    let name = 'Esteban';
+    let name2;
+
+    expect(name).toBeDefined();
+    expect(name2).toBeUndefined();
+    expect(1+3==4).toBeTruthy();
+    expect(1+3==2).toBeFalsy();
+
+  })
 });
