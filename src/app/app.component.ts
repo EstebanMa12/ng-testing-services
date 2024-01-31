2ce2cb4b044424b45b523eefa66f86879f33c217
EstebanMa12 <desteban.maya@udea.edu.co>
2024-01-31 16:27:30 -0500

    Add RouterLinkWithHref import to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RouterLinkWithHref } from '@angular/router';
 
 import { Calculator } from './calculator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLinkWithHref],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
@@ -16,6 +17,6 @@ export class AppComponent {
 
   ngOnInit() {
     const calculator = new Calculator();
-    
+
   }
 }
